2f078a1ac35477739bb4282cfa8800a9336b46a4
makar4 <makar4@gmx.de>
2023-01-15 20:44:28 +0100

    added typewriter main

--- src/components/home/Main.js
+++ src/components/home/Main.js
@@ -1,10 +1,18 @@
 import "../../styling/home/Main.css";
+import Typewriter from "typewriter-effect";
 
 export default function Main() {
     return (
         <main>
             <div id={"home-introduction"}>
-                <p>Hey</p>
+                <h1>
+                    <Typewriter options={{
+                        autoStart: true,
+                        loop: true,
+                        delay: 50,
+                        strings: ["I am a web developer"]
+                    }} />
+                </h1>
             </div>
         </main>
     )
